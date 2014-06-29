3c6383220d5945988695a981d468c7c610c12efd
Bryan Thompson <conundrum2k@gmail.com>
2014-06-29 17:52:44 -0400

    task three

---
+++ TaskThree/TaskThree.java
@@ -0,0 +1,18 @@
+
+
+public class TaskThree
+{
+    public static void main(String[] args)
+    {
+        int resultOne;
+        double resultTwo;
+        
+        resultOne = 3 % 4 - 10 * 5;
+        resultTwo = 5 + 11 / 2 * 2.0;
+        
+        System.out.print("result One = ");
+        System.out.println(resultOne);
+        System.out.print("result Two = ");
+        System.out.println(resultTwo);
+    }
+}
