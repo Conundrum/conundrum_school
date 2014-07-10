12d4a429a22aac84cd89c5be778ae7319bfe2629
Bryan Thompson <conundrum2k@gmail.com>
2014-07-10 13:09:31 -0400

    done with week 3

--- COMP 102x Lab 03/Lab03.java
+++ COMP 102x Lab 03/Lab03.java
@@ -7,7 +7,9 @@ public class Lab03
     public void loadAnImage() 
     {
         // Please write your code after this line
-        
+        ColorImage image = new ColorImage();
+        Canvas canvas = new Canvas(image.getWidth(), image.getHeight());
+        canvas.add(image);
         
         
         
@@ -16,7 +18,11 @@ public class Lab03
     public void loadTwoImagesOnTheSameCanvas()
     {
         // Please write your code after this line
-        
+        ColorImage imageA = new ColorImage();
+        ColorImage imageB = new ColorImage();
+        Canvas canvas = new Canvas(imageA.getWidth() + imageB.getWidth(), imageA.getHeight());
+        canvas.add(imageA);
+        canvas.add(imageB, imageA.getWidth(), 0);
         
         
         
@@ -25,6 +31,14 @@ public class Lab03
     public void imageAddition() 
     {    
         // Please write your code after this line
+        ColorImage imageA = new ColorImage();
+        ColorImage imageB = new ColorImage();
+        ColorImage imageC = new ColorImage(imageA.getWidth(), imageA.getHeight());
+        imageC = imageC.add(imageA, imageB);
+        Canvas canvas = new Canvas(imageA.getWidth() + imageB.getWidth() + imageC.getWidth() + 20, imageA.getHeight());
+        canvas.add(imageA);
+        canvas.add(imageB, imageA.getWidth() + 10, 0);
+        canvas.add(imageC, imageA.getWidth() + 10 + imageB.getWidth() + 10, 0);
         
         
         
@@ -34,6 +48,14 @@ public class Lab03
     public void imageMultiplication() 
     {
         // Please write your code after this line
+        ColorImage imageA = new ColorImage();
+        ColorImage imageB = new ColorImage();
+        ColorImage imageC = new ColorImage(imageA.getWidth(), imageA.getHeight());
+        imageC = imageC.multiply(imageA, imageB);
+        Canvas canvas = new Canvas(imageA.getWidth() + imageB.getWidth() + imageC.getWidth() + 20, imageA.getHeight());
+        canvas.add(imageA);
+        canvas.add(imageB, imageA.getWidth() + 10, 0);
+        canvas.add(imageC, imageA.getWidth() + 10 + imageB.getWidth() + 10, 0);
         
         
         
